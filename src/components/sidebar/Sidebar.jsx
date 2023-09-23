13916fb8c5f7e8ffa4181abf94f899f1cf6b9b42
sentic90 <princeali5549@gmail.com>
2023-09-23 07:46:40 +0000

    added components

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import './sidebar.scss'
+
+const Sidebar = () => {
+    return ( 
+        <div className="sidebar">
+            <div className="logo">Logo</div>
+            <div className="center">
+                <ul>
+                    <li>dashbaord</li>
+                    <li>dashbaord</li>
+                    <li>dashbaord</li>
+                    <li>dashboard</li>
+                </ul>
+            </div>
+            <div className="top">color options</div>
+        </div>
+     );
+}
+ 
+export default Sidebar;
