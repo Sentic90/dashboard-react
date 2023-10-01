be8fe19f713da4cce870e9bd2266b18ad631a11e
sentic90 <princeali5549@gmail.com>
2023-10-01 14:04:26 +0000

    added featured component & auto render widget

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,12 @@
+
+import './chart.scss'
+
+const chart = () => {
+    return ( 
+        <div className="chart">
+            Chart
+        </div>
+     );
+}
+ 
+export default chart;
--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,22 +1,104 @@
-import { KeyboardArrowUp, PersonOutlineOutlined } from '@mui/icons-material';
-import './widget.scss'
-const Widget = () => {
-    return ( 
-        <div className="widget">
-            <div className="left">
-                <span className="title">USERS</span>
-                <span className="counter">2121</span>
-                <div className="link">See All Users</div>
-            </div>
-            <div className="right">
-                <div className="percentage postive">
-                    <KeyboardArrowUp />
-                    20%
-                </div>
-                <PersonOutlineOutlined className='icon' />
-            </div>
+import {
+  AccountBalanceWalletOutlined,
+  KeyboardArrowUp,
+  MonetizationOnOutlined,
+  PersonOutlineOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+
+import "./widget.scss";
+
+const Widget = ({ type }) => {
+  // temp
+  let amount = 100;
+  let diff = 20;
+  let data;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlineOutlined
+            className="icon"
+            style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2)" }}
+          />
+        ),
+      };
+      break;
+
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View all earnings",
+        icon: (
+          <MonetizationOnOutlined
+            className="icon"
+            style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }}
+          />
+        ),
+      };
+      break;
+
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <ShoppingCartOutlined
+            className="icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218,165,32,0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: (
+          <AccountBalanceWalletOutlined
+            className="icon"
+            style={{
+              color: "purple",
+              backgroundColor: "rgba(128, 0, 128, 0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"} {amount}
+        </span>
+        <div className="link">{data.link}</div>
+      </div>
+      <div className="right">
+        <div className="percentage postive">
+          <KeyboardArrowUp />
+          {diff} %
         </div>
-     );
-}
- 
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
 export default Widget;
