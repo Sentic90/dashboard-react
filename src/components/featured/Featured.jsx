6a423d73aa294247f6eb7cd1beae7f60badf186f
sentic90 <princeali5549@gmail.com>
2023-10-21 11:08:24 +0000

    Development:     - added complete Featured Component     - added Recharts lib     - added Chart Component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,58 +1,56 @@
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { CircularProgressbar } from "react-circular-progressbar";
 
-import "react-circular-progressbar/dist/styles.css"
-import './featured.scss'
-import { MdMoreVert } from 'react-icons/md';
-import { KeyboardArrowUp } from '@mui/icons-material';
+import "react-circular-progressbar/dist/styles.css";
+import "./featured.scss";
+import { MdMoreVert } from "react-icons/md";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 
 const featured = () => {
-
-    let value = 70
-    return (
-      <div className="featured">
-        <div className="top">
-          <h1 className="title">Total Revenue</h1>
-          <MdMoreVert fontSize="meduim " />
+  let value = 70;
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MdMoreVert fontSize="meduim " />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={"70"} text="70%" strokeWidth={3} />
         </div>
-        <div className="bottom">
-          <div className="featuredChart">
-            <CircularProgressbar value={"70"} text="70%" strokeWidth={3} />
-          </div>
-          <p className="title">Total Sales made today</p>
-          <p className="amount">$420</p>
-          <p className="desc">
-            Previous transaction proccesing. Last payments may not be included.
-          </p>
+        <p className="title">Total Sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">
+          Previous transaction proccesing. Last payments may not be included.
+        </p>
 
-          <div className="summary">
-              <div className="item">
-                <div className="itemTitle">Target</div>
-                <div className="itemResult">
-                  <KeyboardArrowUp fontSize="small" />
-                  <div className="resultAmount">$12.4k</div>
-                </div>
-              </div>
-              
-              <div className="item">
-                <div className="itemTitle">Target</div>
-                <div className="itemResult">
-                  <KeyboardArrowUp fontSize="small" />
-                  <div className="resultAmount">$12.4k</div>
-                </div>
-              </div>
-              
-              <div className="item">
-                <div className="itemTitle">Target</div>
-                <div className="itemResult">
-                  <KeyboardArrowUp fontSize="small" />
-                  <div className="resultAmount">$12.4k</div>
-                </div>
-              </div>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDown fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
           </div>
 
+          <div className="item">
+            <div className="itemTitle">Last week</div>
+            <div className="itemResult postive">
+              <KeyboardArrowUp fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult postive">
+              <KeyboardArrowUp fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
         </div>
       </div>
-    );
-}
- 
+    </div>
+  );
+};
+
 export default featured;
